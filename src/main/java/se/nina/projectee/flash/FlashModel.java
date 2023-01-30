977799ae5734f7b8e29460562013bf34a45843ee
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2023-01-30 12:56:20 +0100

    flashmodel getter+setters+constructors

--- src/main/java/se/nina/projectee/flash/FlashModel.java
+++ src/main/java/se/nina/projectee/flash/FlashModel.java
@@ -26,6 +26,43 @@ public class FlashModel{
     @Size(min = 2, max = 20)
     private String name;
 
+    public FlashModel(){}
 
+    public FlashModel(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
