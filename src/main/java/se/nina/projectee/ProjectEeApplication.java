84fce60beb04a551da1071ad80e9cb15004f0db9
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2023-01-30 11:20:43 +0100

    Initial commit

---
+++ src/main/java/se/nina/projectee/ProjectEeApplication.java
@@ -0,0 +1,13 @@
+package se.nina.projectee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/nina/projectee/ProjectEeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nina.projectee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
