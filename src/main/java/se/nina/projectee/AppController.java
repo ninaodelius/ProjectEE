e353bab9fc4a06950703cbe81763f03c67c1a67d
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2023-01-30 13:38:20 +0100

    added appcontroller with getmapping for home + appviewconfig with view for home

--- src/main/java/se/nina/projectee/AppController.java
+++ src/main/java/se/nina/projectee/AppController.java
@@ -1,5 +1,23 @@
 package se.nina.projectee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import se.nina.projectee.flash.FlashModelRepository;
 
+@Controller
 public class AppController {
+
+    private final FlashModelRepository flashModelRepository;
+
+    @Autowired
+    public AppController(FlashModelRepository flashModelRepository){
+        this.flashModelRepository = flashModelRepository;
+    }
+
+    @GetMapping("/home")
+    public String displayHome(){
+        return "home";
+    }
+
 }
--- src/main/java/se/nina/projectee/AppViewConfig.java
+++ src/main/java/se/nina/projectee/AppViewConfig.java
@@ -1,4 +1,16 @@
 package se.nina.projectee;
 
-public class AppViewConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class AppViewConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+    }
 }
